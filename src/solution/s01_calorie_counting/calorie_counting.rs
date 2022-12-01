8d8e19f51c6d6c9c01f31e9a7dcb47d2a9be0a2b
thanakritju <thanakrit.ju.work@gmail.com>
2022-12-01 21:39:36 +0700

    format

--- src/solution/s01_calorie_counting/calorie_counting.rs
+++ src/solution/s01_calorie_counting/calorie_counting.rs
@@ -1,12 +1,10 @@
 use std::collections::BinaryHeap;
 
-
-
 fn calorie_counting(numbers: Vec<Vec<i32>>) -> i32 {
     let mut sums: BinaryHeap<i32> = numbers.iter().map(|n| n.iter().sum()).collect();
     match sums.pop() {
         Some(v) => v,
-        None => 0
+        None => 0,
     }
 }
 
@@ -14,15 +12,15 @@ fn calorie_counting_top_three(numbers: Vec<Vec<i32>>) -> i32 {
     let mut ans = 0;
     let mut sums: BinaryHeap<i32> = numbers.iter().map(|n| n.iter().sum()).collect();
     match sums.pop() {
-        Some(v) => {ans += v},
+        Some(v) => ans += v,
         None => {}
     }
     match sums.pop() {
-        Some(v) => {ans += v},
+        Some(v) => ans += v,
         None => {}
     }
     match sums.pop() {
-        Some(v) => {ans += v},
+        Some(v) => ans += v,
         None => {}
     }
     ans
@@ -36,13 +34,19 @@ mod tests {
 
     #[test]
     fn test_calorie_counting() {
-        let input = load_file_to_vectors(String::from("src/solution/s01_calorie_counting/example.txt"));
+        let input = load_file_to_vectors(String::from(
+            "src/solution/s01_calorie_counting/example.txt",
+        ));
         assert_eq!(calorie_counting(input), 24000);
-        let input = load_file_to_vectors(String::from("src/solution/s01_calorie_counting/input.txt"));
+        let input =
+            load_file_to_vectors(String::from("src/solution/s01_calorie_counting/input.txt"));
         assert_eq!(calorie_counting(input), 69626);
-        let input = load_file_to_vectors(String::from("src/solution/s01_calorie_counting/example.txt"));
+        let input = load_file_to_vectors(String::from(
+            "src/solution/s01_calorie_counting/example.txt",
+        ));
         assert_eq!(calorie_counting_top_three(input), 45000);
-        let input = load_file_to_vectors(String::from("src/solution/s01_calorie_counting/input.txt"));
+        let input =
+            load_file_to_vectors(String::from("src/solution/s01_calorie_counting/input.txt"));
         assert_eq!(calorie_counting_top_three(input), 206780);
     }
 }
