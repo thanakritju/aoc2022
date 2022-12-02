bf94a16d666f2d9e85e479a3ff462d781626a441
thanakritju <thanakrit.ju.work@gmail.com>
2022-12-02 23:08:28 +0700

    day2 part2

--- src/solution/s02_rock_paper_scissors/rps.rs
+++ src/solution/s02_rock_paper_scissors/rps.rs
@@ -35,15 +35,15 @@ fn get_score(key: String) -> i32 {
 
 fn get_score2(key: String) -> i32 {
     let mut map: HashMap<String, i32> = HashMap::new();
-    map.insert("A X".to_string(), 1 + 3);
-    map.insert("A Y".to_string(), 2 + 6);
-    map.insert("A Z".to_string(), 3 + 0);
-    map.insert("B X".to_string(), 1 + 0);
-    map.insert("B Y".to_string(), 2 + 3);
-    map.insert("B Z".to_string(), 3 + 6);
-    map.insert("C X".to_string(), 1 + 6);
-    map.insert("C Y".to_string(), 2 + 0);
-    map.insert("C Z".to_string(), 3 + 3);
+    map.insert("A X".to_string(), 0 + 3);
+    map.insert("A Y".to_string(), 3 + 1);
+    map.insert("A Z".to_string(), 6 + 2);
+    map.insert("B X".to_string(), 0 + 1);
+    map.insert("B Y".to_string(), 3 + 2);
+    map.insert("B Z".to_string(), 6 + 3);
+    map.insert("C X".to_string(), 0 + 2);
+    map.insert("C Y".to_string(), 3 + 3);
+    map.insert("C Z".to_string(), 6 + 1);
     match map.get(&key) {
         Some(score) => *score,
         None => 0,
