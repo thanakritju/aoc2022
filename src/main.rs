ea6da26bb2c4da0f09566c7ae495a51af4450fc6
thanakritju <thanakrit.ju.work@gmail.com>
2022-12-04 15:51:17 +0700

    Add cli program

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ mod utils;
 
 use clap::Parser;
 use solution::s01::calorie_counting::solution_day1_part1;
+use solution::s01::calorie_counting::solution_day1_part2;
+use solution::s02::rps::solution_day2_part1;
+use solution::s02::rps::solution_day2_part2;
+use solution::s03::reorganize::solution_day3_part1;
+use solution::s03::reorganize::solution_day3_part2;
 
 #[derive(Parser)]
 struct Cli {
@@ -12,4 +17,26 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+
+    match args.pattern.as_str() {
+        "day1part1" => {
+            println!("{}", solution_day1_part1(args.path))
+        }
+        "day1part2" => {
+            println!("{}", solution_day1_part2(args.path))
+        }
+        "day2part1" => {
+            println!("{}", solution_day2_part1(args.path))
+        }
+        "day2part2" => {
+            println!("{}", solution_day2_part2(args.path))
+        }
+        "day3part1" => {
+            println!("{}", solution_day3_part1(args.path))
+        }
+        "day3part2" => {
+            println!("{}", solution_day3_part2(args.path))
+        }
+        _ => println!("In valid pattern"),
+    }
 }
