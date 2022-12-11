5588bc79be53ac6e693d6538adce2f9ec5f93a8d
thanakritju <thanakrit.ju.work@gmail.com>
2022-12-11 10:56:55 +0700

    day1

--- src/solution/s10/main.rs
+++ src/solution/s10/main.rs
@@ -1,8 +1,10 @@
+use std::collections::VecDeque;
+
 use crate::utils::load_file::load_file_to_string_vectors;
 
 pub fn solution_day10_part1(path: std::path::PathBuf) -> i32 {
     let input = load_file_to_string_vectors(path);
-    let registers: [i32; 220] = process(input);
+    let registers: [i32; 221] = process(input);
     vec![20, 60, 100, 140, 180, 220]
         .into_iter()
         .map(|cycle| signal_strength(registers[cycle - 1], cycle as i32))
@@ -20,13 +22,17 @@ fn signal_strength(register: i32, cycle: i32) -> i32 {
 fn process<const LEN: usize>(actions: Vec<String>) -> [i32; LEN] {
     let mut arr = [1; LEN];
     let mut ai = 0;
-    let mut pause = false;
     let mut num = 0;
+    let mut q: VecDeque<i32> = VecDeque::from([0, 0]);
     for i in 0..LEN {
         if i > 0 {
             arr[i] = arr[i - 1];
         }
-        if !pause {
+        println!("{} {:?}", i, q);
+        let n = q.pop_front().expect("No data");
+        arr[i] = arr[i] + n;
+
+        if q.iter().all(|e| *e == 0) {
             let action = actions.get(ai % actions.len()).expect("no data");
             if action.starts_with("addx") {
                 let splited: Vec<&str> = action.split("addx ").collect();
@@ -35,15 +41,14 @@ fn process<const LEN: usize>(actions: Vec<String>) -> [i32; LEN] {
                     .expect("no data")
                     .parse()
                     .expect("parse error");
-                arr[i] += num;
-                pause = true;
+                q.push_back(num);
                 ai += 1;
             } else {
-                pause = false;
+                q.push_back(0);
                 ai += 1;
             }
         } else {
-            pause = true;
+            q.push_back(0);
         }
     }
     arr
@@ -58,12 +63,22 @@ mod tests {
     #[test]
     fn test_process() {
         let actions = load_file_to_string_vectors("src/solution/s10/small_example.txt");
-        let registers: [i32; 5] = process(actions);
+        let registers: [i32; 6] = process(actions);
+        assert_eq!(registers, [1, 1, 1, 4, 4, -1]);
         assert_eq!(registers[0], 1);
         assert_eq!(registers[1], 1);
-        assert_eq!(registers[2], 4);
+        assert_eq!(registers[2], 1);
         assert_eq!(registers[3], 4);
-        assert_eq!(registers[4], -1);
+        assert_eq!(registers[4], 4);
+        assert_eq!(registers[5], -1);
+        let actions = load_file_to_string_vectors("src/solution/s10/example.txt");
+        let registers: [i32; 221] = process(actions);
+        assert_eq!(registers[20], 21);
+        assert_eq!(registers[60], 19);
+        assert_eq!(registers[100], 18);
+        assert_eq!(registers[140], 21);
+        assert_eq!(registers[180], 16);
+        assert_eq!(registers[219], 18);
     }
 
     #[test]
@@ -74,7 +89,7 @@ mod tests {
         );
         assert_eq!(
             solution_day10_part1(PathBuf::from("src/solution/s10/input.txt")),
-            0
+            14720
         );
         assert_eq!(
             solution_day10_part2(PathBuf::from("src/solution/s10/example.txt")),
